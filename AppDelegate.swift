2e2e693e2a65cbc95a751d53054aa7fc3e8a3e26
Tatsuhiko Arai <tatsu@tatsu.com>
2016-04-22 21:44:16 +0900

    Replace AppDelegate

---
+++ AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  AVBasicVideoOutput
+//
+//  Created by Tatsuhiko Arai on 4/22/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
